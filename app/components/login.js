7699f6e6da6f2c01008ca2679ffb1bdc141a3bfb
DillJoker5 <79213574+DillJoker5@users.noreply.github.com>
2022-03-30 09:50:36 -0500

    Finished display for login and register page

--- app/components/login.js
+++ app/components/login.js
@@ -1,19 +1,6 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class RegisterComponent extends Component {
     @service router;
-    @tracked username = this.get('login.username');
-    @tracked password = this.get('login.password');
-
-    get isDisabled() {
-        if(this.username === '' || this.password === '') {
-            return "true"
-            console.log('true')
-        }
-        console.log('null')
-        return null;
-    }
 }
