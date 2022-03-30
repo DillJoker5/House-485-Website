27271658395bc33aeec52cac22d2c84f64450e99
DillJoker5 <79213574+DillJoker5@users.noreply.github.com>
2022-03-29 20:13:14 -0500

    Starting button disabling logic

--- app/components/login.js
+++ app/components/login.js
@@ -1,3 +1,19 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class LoginComponent extends Component {}
+export default class RegisterComponent extends Component {
+    @service router;
+    @tracked username = this.username;
+    @tracked password = this.password;
+
+    get isDisabled() {
+        if(username === '' || password === '') {
+            return "true"
+            console.log('true')
+        }
+        console.log('null')
+        return null;
+    }
+}
--- app/components/register.js
+++ app/components/register.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class RegisterComponent extends Component {}
+export default class RegisterComponent extends Component {
+    @service router;
+    @tracked username = '';
+    @tracked password = '';
+
+    @action isDisabled() {}
+}
